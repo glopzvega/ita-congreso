098260a73b53449f68c6cb576f2a826b88083e7d
GLVEGA <mcgalv@gmail.com>
2018-04-07 15:09:32 -0500

    Load Index View on Registro

--- registro/views.py
+++ registro/views.py
@@ -7,20 +7,13 @@ from .forms import RegistroModelForm
 
 def index(request):
 
-	context = {
-		
-	}
-	return render(request, "registro/index.html", context)
-
-def registrar(request):
-
 	if request.method == 'POST':
 		
 		form = RegistroModelForm(request.POST, request.FILES)
 		
 		if form.is_valid():
 			# commit=False tells Django that "Don't send this to database yet.
-            # I have more things I want to do with it."
+			# I have more things I want to do with it."
 			registro = form.save(commit=False)
 			registro.save()
 			
@@ -33,7 +26,30 @@ def registrar(request):
 		"form" : form
 	}
 
-	return render(request, "registro/formulario.html", context)	
+	return render(request, "registro/index.html", context)
+
+def registrar(request):
+
+	if request.method == 'POST':
+		
+		form = RegistroModelForm(request.POST, request.FILES)
+		
+		if form.is_valid():
+			# commit=False tells Django that "Don't send this to database yet.
+			# I have more things I want to do with it."
+			registro = form.save(commit=False)
+			registro.save()
+			
+			return redirect('confirmacion')
+			
+	else:
+		form = RegistroModelForm()
+
+	context = {
+		"form" : form
+	}
+
+	return render(request, "registro/index.html", context)	
 
 def confirmacion(request):
 
